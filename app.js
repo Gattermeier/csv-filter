8156fe751e05350d987958e9f1a3cd9c38616667
Gattermeier <matthias.gattermeier@gmail.com>
2015-03-26 11:03:21 -0400

    Bug Fixes and Features
    Fixed execution logic & added progress bar.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var fs = require('fs');
 var csv = require('ya-csv');
 var converter = require('json-2-csv');
+var ProgressBar = require('progress');
 
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -23,82 +24,93 @@ if (argv.filter) {
     var filter = 'Email';
 }
 
-// if stop true we won't filter but abort
-var stop = false;
-
 
 if (!sourcefile || !filterfile) {
     console.log('Usage: node app.js [-i sourcefile.csv] [-f filterfile.csv] [-o outputfile.csv] [--filter: FIELD]')
     console.log('Default output file: output.csv | Default filter value: Email')
 } else {
+
     filterRecords();
 }
 
-function filterRecords(){
 
-    var records = [];
+
+
+function filterRecords(){
+    console.log('Please wait.. Depending on file size, this can take a while.');
+
+    var recordsArr = [];
     var filterArr = [];
     var results = [];
+    var readCount = 0;
 
     var recordsReader = csv.createCsvFileReader(sourcefile, { columnsFromHeader: true });
     var filterReader = csv.createCsvFileReader(filterfile, { columnsFromHeader: true });
 
 
     recordsReader.addListener('data', function(data) {
-        records.push(data);
+        if (data[filter]) {
+            recordsArr.push(data);
+        }
     });
 
     filterReader.addListener('data', function(data) {
-        if (!data[filter]) {
-            stop = true;
-            return false;
-        } else {
+        if (data[filter]) {
             filterArr.push(data);
         }
     });
 
     recordsReader.on('end', function(){
+        readCount++;
+        readerEnd(readCount);
+    });
 
-        filterReader.on('end',function(){
+    filterReader.on('end',function(){
+        readCount++;
+        readerEnd(readCount);
+    })
 
-            if (!stop) {
+    function readerEnd(readCount) {
+        var bar = new ProgressBar('Processing [:bar] :percent ETAS: :etas', {
+            total: filterArr.length,
+            complete: '=',
+            incomplete: ' '
+        });
+
+        if (readCount >=2) {
 
                 for (i = 0; i < filterArr.length; i++) {
                     var find = filterArr[i][filter];
-                    for (j = 0; j < records.length; j++) {
-                        if (find === records[j][filter]) {
-
-                            results.push(records[j]);
-
+                    for (j = 0; j < recordsArr.length; j++) {
+                        if (find === recordsArr[j][filter]) {
+                            results.push(recordsArr[j]);
+                            //console.log('Matching Record. Filter position '+i+' of ' + filterArr.length +' and Records position '+j+' of '+ recordsArr.length);
+                            recordsArr.splice(j, 1);
                         }
                     }
+                    bar.tick();
                 }
+            console.log('loop end');
 
                 var json2csvCallback = function (err, csv) {
                     if (err) {
                         console.log(err);
                     }
-
                     fs.writeFile(outputfile, csv, function (err) {
                         if (err) {
                             console.log(err);
                         }
-
                         console.log('Saved: '+ outputfile);
                     });
 
                 };
 
                 converter.json2csv(results, json2csvCallback);
+        }
+    }
 
-            } else {
-                console.log('Error, filter value '+filter+' not found in (some) entries of '+filterfile);
-            }
-
-        })
-
-
-    });
 
 }
+
+
 
